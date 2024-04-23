f6c4c430facfc787aed784e653c74d2a59e1520b
Muharax <adriangajda@o2.pl>
2024-04-23 16:10:58 +0200

    Updates

---
+++ assets/Cube-6faf64ff.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-a3391ecc.js";const o=e({__name:"Cube",setup(t){return(n,r)=>" Cube "}});export{o as default};
--- assets/Cube-704c0a99.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-a1d9cace.js";const o=e({__name:"Cube",setup(t){return(n,r)=>" Cube "}});export{o as default};
